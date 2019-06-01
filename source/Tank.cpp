933c72e78f420f34919b2791673267dbecdf91e0
Daniel White <xt1zer@outlook.com>
2019-06-01 09:42:47 +0300

    Added animation

--- source/Tank.cpp
+++ source/Tank.cpp
@@ -6,7 +6,7 @@ Tank::Tank(const float &x, const float &y, const int &width, const int &height, 
 
     mTexture.loadFromFile(mFile);
     mSprite.setTexture(mTexture);
-    mSprite.setTextureRect(sf::IntRect(0, 0, mWidth, mHeight));
+    mSprite.setTextureRect(sf::IntRect(0, 1, mWidth, mHeight));
     mSprite.setPosition(mX, mY);
 }
 
@@ -17,3 +17,18 @@ void Tank::set_sprite_position(const float &x, const float &y) {
 }
 
 const sf::Sprite Tank::get_sprite() const { return mSprite; }
+
+void Tank::animate(const sf::Time &time, const sf::Vector2f &movement) {
+    mCurrentFrame += 0.00001f * time.asMicroseconds();
+    if (mCurrentFrame >= 2)
+        mCurrentFrame -= 2;
+
+    if (movement.x > 0.01f)
+        mSprite.setTextureRect(sf::IntRect(96 + 16 * (int)mCurrentFrame, 0, 13, 13));
+    else if (movement.x < -0.01f)
+        mSprite.setTextureRect(sf::IntRect(33 + 16 * (int)mCurrentFrame, 0, 13, 13));
+    else if (movement.y > 0.01f)
+        mSprite.setTextureRect(sf::IntRect(64 + 16 * (int)mCurrentFrame, 0, 13, 13));
+    else if (movement.y < -0.01f)
+        mSprite.setTextureRect(sf::IntRect(16 * (int)mCurrentFrame, 1, 13, 13));
+}
