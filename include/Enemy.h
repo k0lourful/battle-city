61f7b179f6f70bc2bafe1e531275572f9361ec4b
Daniel White <xt1zer@outlook.com>
2019-06-15 00:11:19 +0300

    Bots now shoot; separated update function

--- include/Enemy.h
+++ include/Enemy.h
@@ -4,10 +4,13 @@
 class Enemy : public Tank {
 private:
     float timeBeforeMoving;
+    float timeBeforeShot;
 
 public:
     Enemy(const float &x, const float &y);
     Enemy(const Enemy &copy) : Enemy(copy.mX, copy.mY){}
 
     void move(const sf::Int64 &time);
+    void shoot(const float &time);
+    void update(const sf::Int64 &time, Map &map, const bool &collision);
 };
--- source/Enemy.cpp
+++ source/Enemy.cpp
@@ -1,7 +1,7 @@
 #include <Enemy.h>
 
 Enemy::Enemy(const float &x, const float &y)
-    : Tank(x, y, 39, 39, "battle-city-src/media/enemySprites.png"), timeBeforeMoving(0.f) {
+    : Tank(x, y, 39, 39, "battle-city-src/media/enemySprites.png"), timeBeforeMoving(0.f), timeBeforeShot(0.f) {
 
     bullet.playerShot = false;
 }
@@ -10,7 +10,7 @@ void Enemy::move(const sf::Int64 &time) {
     mSpeed = 0.1f;
     timeBeforeMoving += time;
 
-    if (timeBeforeMoving > 1500) {
+    if (timeBeforeMoving > 1000 + rand() % 501) {
         mDir = rand() % 4;
         timeBeforeMoving = 0.f;
     }
@@ -39,4 +39,29 @@ void Enemy::move(const sf::Int64 &time) {
     if (mCollision) tank_interaction();
     mX += mDx * time;
     mY += mDy * time;
+}
+
+void Enemy::shoot(const float &time) {
+    timeBeforeShot += time;
+
+    if (timeBeforeShot > 1000.f) {
+        if (!bullet.present)
+            if (rand() % 2)
+                bullet.present = true;
+
+        timeBeforeShot = 0.f;
+    }
+}
+
+void Enemy::update(const sf::Int64 &time, Map &map, const bool &collision) {
+    mCollision = collision;
+    move(time);
+
+    mSpeed = 0.f;
+    mSprite.setPosition(mX, mY);
+    animate(time);
+    map_interaction(map);
+
+    bullet.update(map, time, mX, mY, mDir);
+    shoot(time);
 }
