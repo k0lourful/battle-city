7bc7c7b4c466d638e40d80560a62fdae4dbdf992
Daniel White <xt1zer@outlook.com>
2019-06-15 00:15:38 +0300

    Added start and end game messages; frags counter; completed game functionality

--- source/Application.cpp
+++ source/Application.cpp
@@ -2,11 +2,13 @@
 #include <Application.h>
 
 Application::Application()
-    : mWindow(sf::VideoMode(768, 672), "Battle City"), gameOver(false) {
-    packOfEnemies = new Enemy[1]{Enemy(52,30)};
+    : mWindow(sf::VideoMode(720, 672), "Battle City"), gameOver(false), gameStarted(false),
+    msgStart(90, 330, "Press \'Enter\' to start"), msgOver(250, 300, "Game over"),
+    msgLost(260, 350, "You lost"), msgWon(265, 350, "You won"), frags(0) {
 
-    float currentFrame(0.f);
     sf::Clock clock;
+
+    packOfEnemies = new Enemy[4]{ Enemy(52,31), Enemy(147,391), Enemy(532,391), Enemy(628,31) };
 
     while (mWindow.isOpen()) {
         sf::Int64 time = clock.getElapsedTime().asMicroseconds();
@@ -14,7 +16,12 @@ Application::Application()
         time /= 800;
 
         process_events();
-        update(time);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+            gameStarted = true;
+
+        if (gameStarted && !gameOver)
+            update(time);
         render();
     }
 }
@@ -31,21 +38,89 @@ void Application::process_events() {
 }
 
 void Application::update(const sf::Int64 &time) {
-    const bool collision = mPlayer.get_sprite().getGlobalBounds().intersects(packOfEnemies[0].get_sprite().getGlobalBounds());
-    mPlayer.update(time, map, collision);
-    packOfEnemies[0].update(time, map, collision);
+    for (int i(0); i < 4; ++i)
+        if (!packOfEnemies[i].life)
+            ++frags;
+        else {
+            frags = 0;
+            break;
+        }
+
+    if (frags == 4)
+        gameOver = true;
+
+    if (!mPlayer.life)
+        gameOver = true;
+
+    bool collision;
+    for (int i(0); i < 4; ++i) {
+        collision = mPlayer.mSprite.getGlobalBounds().intersects(packOfEnemies[i].mSprite.getGlobalBounds());
+        if (collision)
+            break;
+    }
+
+    if (mPlayer.life)
+        mPlayer.update(time, map, collision);
+
+    for (int i(0); i < 4; ++i) {
+        if (packOfEnemies[i].life) {
+            packOfEnemies[i].update(time, map, collision);
+
+            if (packOfEnemies[i].bullet.mSprite.getGlobalBounds().intersects(mPlayer.bullet.mSprite.getGlobalBounds())
+                && packOfEnemies[i].bullet.present) {
+                mPlayer.collapse();
+                packOfEnemies[i].bullet.present = false;
+            }
+
+            if (packOfEnemies[i].bullet.mSprite.getGlobalBounds().intersects(mBase.mSprite.getGlobalBounds())
+                && packOfEnemies[i].bullet.present) {
+                mBase.life = false;
+                gameOver = true;
+            }
+            if (mPlayer.bullet.mSprite.getGlobalBounds().intersects(packOfEnemies[i].mSprite.getGlobalBounds())
+                && mPlayer.bullet.present) {
+                packOfEnemies[i].collapse();
+                mPlayer.bullet.present = false;
+            }
+        }
+    }
+
+    if (mPlayer.bullet.mSprite.getGlobalBounds().intersects(mBase.mSprite.getGlobalBounds())
+        && mPlayer.bullet.present) {
+        mBase.life = false;
+        gameOver = true;
+    }
 }
 
 void Application::render() {
     mWindow.clear();
     map.draw(mWindow);
-    mWindow.draw(mPlayer.get_sprite());
-    if(mPlayer.bullet.present) mWindow.draw(mPlayer.bullet.mSprite);
+    if (mPlayer.life)
+        mWindow.draw(mPlayer.mSprite);
+    if (mPlayer.bullet.present) mWindow.draw(mPlayer.bullet.mSprite);
 
-    mWindow.draw(packOfEnemies[0].get_sprite());
+    for (int i(0); i < 4; ++i) {
+        if (packOfEnemies[i].bullet.present)
+            mWindow.draw(packOfEnemies[i].bullet.mSprite);
 
-    mWindow.draw(mBase.get_sprite());
+        if (packOfEnemies[i].life)
+            mWindow.draw(packOfEnemies[i].mSprite);
+    }
+
+    if (mBase.life)
+        mWindow.draw(mBase.mSprite);
+
+    if (!gameStarted)
+        msgStart.print(mWindow);
+
+    if (gameOver) {
+        msgOver.print(mWindow);
+        if (!mBase.life || !mPlayer.life)
+
+            msgLost.print(mWindow);
+        else
+            msgWon.print(mWindow);
+    }
+
     mWindow.display();
 }
-
-
